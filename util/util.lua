671e61496395dc4afbac5e336735b64561fd7579
Tobias Arens <t.arens@fz-juelich.de>
2023-12-15 10:18:18 +0900

    add util file

---
+++ util/util.lua
@@ -0,0 +1,44 @@
+require("lua-string")
+
+function table.copy(t, copy_function)
+    copy_function = copy_function or function(v) return v end
+    local cpy = {}
+    for k, v in pairs(t) do
+        cpy[k] = copy_function(v)
+    end
+    return cpy
+end
+
+function table.maxKV(t)
+    local maxk
+    local maxv
+    for k, v in pairs(t) do
+        if not maxk then
+            maxk = k
+            maxv =v
+        elseif v > maxv then
+            maxk = k
+            maxv = v
+        end
+    end
+    return {maxk, maxv}
+end
+
+function table.max(t)
+    return table.maxKV(t)[2]
+end
+
+function table.argmax(t)
+    return table.maxKV(t)[1]
+end
+
+
+function string.count(str, char)
+    local count  = 0
+    for i = 1, string.len(str) do
+        if str[i] == char then
+            count = count + 1
+        end
+    end
+    return count
+end
